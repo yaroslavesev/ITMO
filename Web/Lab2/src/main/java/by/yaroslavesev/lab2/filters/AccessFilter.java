1babff8f211354c98ac8ae05a5f84c22c5436bfc
yaroslavesev <yaroslavesev88@gmail.com>
2024-11-06 12:17:14 +0300

    added filter

--- Web/Lab2/src/main/java/by/yaroslavesev/lab2/filters/AccessFilter.java
+++ Web/Lab2/src/main/java/by/yaroslavesev/lab2/filters/AccessFilter.java
@@ -1,4 +1,24 @@
 package by.yaroslavesev.lab2.filters;
 
-public class AccessFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class AccessFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getServletPath();
+        if (path.equals("/controller") || path.endsWith(".css") || path.endsWith(".js")  || path.endsWith(".png")) {
+            chain.doFilter(request, response);
+        } else {
+            request.getRequestDispatcher("/controller").forward(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {}
 }
